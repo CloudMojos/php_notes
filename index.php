ec941b5a27c8beaa2ff908bdb5dd0e104fac0de9
CloudMojos <joshuel.simbulan@gmail.com>
2023-06-30 18:57:04 +0800

    more math functions

--- index.php
+++ index.php
@@ -8,17 +8,31 @@
 	</head>
 	<body>
 		<form action="index.php" method="post">
-			<label for="x">quantity: </label>
+			<label for="x">x: </label>
 			<input name="x" type="text">
+			<label for="y">y: </label>
+			<input name="y" type="text">
+			<label for="z">z: </label>
+			<input name="z" type="text">
 			<input type="submit" value="total">
 		</form>
 	</body>
 </html>
 <?php 
 	$x = $_POST["x"];
+	$y= $_POST["y"];
+	$z = $_POST["z"];
 	$total = null;
 
-	$total = round($x);
-
+	// $total = abs($x);
+	// $total = round($x);
+	// $total = floor($x);
+	// $total = ceil($x);
+	// $total = sqrt($x);
+	// $total = pow($x, $y);
+	// $total = max($x, $y, $z);
+	// $total = min($x, $y, $z);
+	// $total = pi();
+	// $total = rand(1, 6);
 	echo $total;
 ?>
